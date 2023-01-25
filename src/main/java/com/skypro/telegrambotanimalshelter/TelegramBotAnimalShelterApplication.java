fb5b37bf4352b07259374b5303051780cc293f5b
arste <a1ekseevartem.ru@gmail.com>
2023-01-25 16:14:07 +0400

    the first commit. creating a project.

---
+++ src/main/java/com/skypro/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.telegrambotanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/telegrambotanimalshelter/TelegramBotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.telegrambotanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
